98434323a7c9bf2f312cb013c61048002a1a83f4
Freddie <freddiex06@outlook.com>
2024-05-02 20:31:29 -0700

    'NumberAnalyer'

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine(8 + 2 * 5);
+Console.WriteLine((8 + 2) * 5);
+Console.WriteLine(8 + 2 * (5 - 3));
+Console.WriteLine(20 / 2 - 3 * 2);
---
+++ NUMBER ANALYZER/Program.cs
@@ -0,0 +1,57 @@
+﻿//ask user to input number between 1-100
+using System.ComponentModel.Design;
+bool playagain = true;
+
+while (true)
+{ 
+    Console.WriteLine("Hello User, What is your name?");
+string username = Console.ReadLine();
+
+{
+    Console.WriteLine("Hello, " + username + " please enter a number between 1 - 100?");
+    string numberselected = Console.ReadLine();
+
+        int number = int.Parse(numberselected);
+
+        if (number <= 60 && number % 2 != 0)
+        {
+            Console.WriteLine("Odd and less than 60.");
+        }
+        else if (number >= 60 && number % 2 != 0)
+
+        {
+            Console.WriteLine("Odd and greater than 60.");
+
+        }
+        else if (number >= 60 && number % 2 == 0)
+
+        {
+            Console.WriteLine("Even and greater than 60.");
+        }
+        else if (number >= 26 && number <= 60 && number % 2 == 0)
+        {
+            Console.WriteLine("Even and between 26 and 60 inclusive.");
+        }
+        else if (number >= 2 && number <= 24 && number % 2 == 0)
+        {
+            Console.WriteLine("Even and less than 25.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input! Please enter number between 1-100");
+        }
+        {
+            Console.WriteLine();
+        }
+        {
+            Console.Write("Would you like to play again? Enter (Y/N?)");
+            Console.WriteLine();
+            string answer = Console.ReadLine();
+            if (answer == "N")
+                {
+                playagain = false;
+                break;
+                }
+        }
+    }
+}
