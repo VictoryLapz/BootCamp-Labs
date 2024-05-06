b92263b4be5b7a0ef8fce6080070a0cf878c69e3
Freddie <freddiex06@outlook.com>
2024-05-06 08:58:11 -0700

    'roomcalculator'

--- Room Calculator/Room Calculator/Program.cs
+++ Room Calculator/Room Calculator/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine(8 + 2 * 5);
+﻿///Calculator
+Console.WriteLine(8 + 2 * 5);
 Console.WriteLine((8 + 2) * 5);
 Console.WriteLine(8 + 2 * (5 - 3));
 Console.WriteLine(20 / 2 - 3 * 2);
