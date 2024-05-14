8cc1f3c1e4b100187910eb1e976c1e78e421ee4b
Freddie <freddiex06@outlook.com>
2024-05-13 19:21:05 -0700

    'BasicFor-Loops'

---
+++ Basic For-Loops/Basic For-Loops/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.CodeAnalysis;
+
+
+Console.WriteLine("Please enter a number?");
+int numberpicked = int.Parse(Console.ReadLine());
+  
+int sum = 0;
+for (int a = 1; a <= numberpicked; a++) 
+{ 
+    sum += a;
+}
+{
+    Console.WriteLine($"The total sum equals {sum}");
+}
