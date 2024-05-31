94628f7e7a0f5c24ca6718892605bc136bceaa56
Freddie <freddiex06@outlook.com>
2024-05-30 21:18:37 -0700

    MovieDataBase

---
+++ Movie Database/Class1.cs
@@ -0,0 +1,49 @@
+﻿using MovieLab;
+
+
+    List<Movie> movieList = new List<Movie>
+{
+    new Movie("Friday", "Comedy"),
+    new Movie("Boss Baby", "Children"),
+    new Movie("New Jack City", "Drama"),
+    new Movie("Halloween", "Horror"),
+    new Movie("Freaky Friday", "Children"),
+    new Movie("Dobuel 07", "Action"),
+    new Movie("Rush Hour", "Comedy"),
+    new Movie("Life", "Comedy"),
+    new Movie("Taken", "Drama"),
+    new Movie("Top Gun", "Action"),
+};
+
+do
+{
+
+    Console.WriteLine("Welcome to the Movie List Application");
+    Console.WriteLine("There are 10 movies in this list.");
+    Console.WriteLine("What category are you interested in?");
+    Console.WriteLine();
+
+    string userSelection = Console.ReadLine();
+
+    foreach (Movie movie in movieList)
+    {
+        if (movie.Category == userSelection)
+        {
+            Console.WriteLine($"{movie.Title}");
+        }
+
+    }
+    Console.WriteLine("Would you like to continue? Y/N");
+    string Response = Console.ReadLine().ToLower();
+    if (Response != "y")
+    {
+        break; // Exit the loop if user enters anything other than 'Y'
+    }
+}while (true);
+
+
+
+
+
+
+
---
+++ Movie Database/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace MovieLab;
+
+public class Movie
+{
+    public string Title { get; set; }
+
+    public string Category { get; set; }
+
+    public Movie(string title, string category)
+
+    {
+        Title = title;
+        Category = category;
+    }
+}
+
+
+
+
+
+
